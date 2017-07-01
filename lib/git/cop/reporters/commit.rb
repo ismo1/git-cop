7057d95483f240a67d254ddde3d54a6c5b6592b4
Brooke Kuhlmann <brooke@alchemists.io>
2017-07-01 17:40:53 -0600

    Added commit reporter.
    - Will be used to report issues related to a specific commit.

---
+++ lib/git/cop/reporters/commit.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+module Git
+  module Cop
+    module Reporters
+      class Commit
+        def initialize commit:, cops: []
+          @commit = commit
+          @cops = cops
+        end
+
+        def to_s
+          "#{commit.sha} (#{commit.author_name}, #{commit.author_date_relative}): " \
+          "#{commit.subject}\n#{cop_report}\n"
+        end
+
+        private
+
+        attr_reader :commit, :cops
+
+        def cop_report
+          cops.reduce("") { |report, cop| report + Cop.new(cop).to_s }
+        end
+      end
+    end
+  end
+end
